e63399cfdd9b67a8e4d94acb53d682e3b401f4a4
louiszhenyean <louiszhenyean@gmail.com>
2020-04-25 22:42:27 +0800

    Changed requirement, starting coding script

---
+++ start-bot.py
@@ -0,0 +1,14 @@
+import time
+import sys
+
+def main(argv):
+    period = 10
+
+    while True:
+        print "period"
+        time.sleep(period)
+
+if __name__ == "__main__":
+        main(sys.argv[1:])
+
+        
