d6bec775cd894f428cb95b1db368eec151251383
louiszhenyean <louiszhenyean@gmail.com>
2020-12-17 22:43:33 +0800

    get price value

--- test.py
+++ test.py
@@ -16,6 +16,8 @@ api_secret  = os.environ.get('API_SECRET')
 client      = Client(api_key, api_secret)
 
 while True:
-    price = client.get_symbol_ticker(symbol=symbol)
-    print(list(list(price.items())[1]))
+    price_response = client.get_symbol_ticker(symbol=symbol)
+    price = float(list(list(price_response.items())[1])[1]) # Get Price Value
+    print(price_response)
+    print(price)
     time.sleep(1)
