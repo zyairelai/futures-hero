5b4ff31335e59e922f27780e2bfc94dc7023e303
louiszhenyean <louiszhenyean@gmail.com>
2020-12-17 22:36:00 +0800

    get price per sec

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
-import keys
+import os
 import sys
-from binance.client import Client
-import datetime
 import time
+import json
+from binance.client import Client
 
 # start = time.time()
 # print(f"{time.time() - start} seconds\n")
+
+symbol  = "ETHUSDT"
+core    =  500
+
+# Get environment variables
+api_key     = os.environ.get('API_KEY')
+api_secret  = os.environ.get('API_SECRET')
+client      = Client(api_key, api_secret)
+
+while True:
+    price = client.get_symbol_ticker(symbol=symbol)
+    print(list(list(price.items())[1]))
+    time.sleep(1)
