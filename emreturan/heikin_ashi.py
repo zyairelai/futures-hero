5dfe63608ec6655c8829157c78121c19177c4403
louiszhenyean <louiszhenyean@gmail.com>
2020-12-31 19:03:20 +0800

    heikin-ashi works

---
+++ emreturan/heikin_ashi.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def heikin_ashi(df):
+    # heikin_ashi_df = pd.DataFrame(index=df.index.values, columns=['open', 'high', 'low', 'close'])
+    heikin_ashi_df = pd.DataFrame(columns=['open', 'high', 'low', 'close'])
+    
+    heikin_ashi_df['close'] = (df['open'] + df['high'] + df['low'] + df['close']) / 4
+    
+    for i in range(len(df)):
+        if i == 0:
+            heikin_ashi_df.iat[0, 0] = df['open'].iloc[0]
+        else:
+            heikin_ashi_df.iat[i, 0] = (heikin_ashi_df.iat[i-1, 0] + heikin_ashi_df.iat[i-1, 3]) / 2
+        
+    heikin_ashi_df['high'] = heikin_ashi_df.loc[:, ['open', 'close']].join(df['high']).max(axis=1)
+    
+    heikin_ashi_df['low'] = heikin_ashi_df.loc[:, ['open', 'close']].join(df['low']).min(axis=1)
+    
+    return heikin_ashi_df
+    
