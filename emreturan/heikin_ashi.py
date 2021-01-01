62c77fee52f389c5adaccb7060d537d402932dc7
louiszhenyean <louiszhenyean@gmail.com>
2021-01-01 09:58:50 +0800

    heikin-ashi works

--- emreturan/heikin_ashi.py
+++ emreturan/heikin_ashi.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-def heikin_ashi(df):
-    # heikin_ashi_df = pd.DataFrame(index=df.index.values, columns=['open', 'high', 'low', 'close'])
+def heikin_ashi_calculations(df):
     heikin_ashi_df = pd.DataFrame(columns=['open', 'high', 'low', 'close'])
-    
     heikin_ashi_df['close'] = (df['open'] + df['high'] + df['low'] + df['close']) / 4
     
     for i in range(len(df)):
@@ -13,8 +11,11 @@ def heikin_ashi(df):
             heikin_ashi_df.iat[i, 0] = (heikin_ashi_df.iat[i-1, 0] + heikin_ashi_df.iat[i-1, 3]) / 2
         
     heikin_ashi_df['high'] = heikin_ashi_df.loc[:, ['open', 'close']].join(df['high']).max(axis=1)
-    
     heikin_ashi_df['low'] = heikin_ashi_df.loc[:, ['open', 'close']].join(df['low']).min(axis=1)
     
     return heikin_ashi_df
     
+df = pd.read_csv("ohcl.txt", sep="   ", names=['open','high','low','close'],engine='python')
+print(df)
+heikin_ashi_df = heikin_ashi_calculations(df)
+print(heikin_ashi_df)
