5decf15101833ef2e971fbe0158d34fdcc17709d
louiszhenyean <louiszhenyean@gmail.com>
2021-01-04 23:28:26 +0800

    get proper avg

--- 2-current-trend.py
+++ 2-current-trend.py
@@ -23,42 +23,41 @@ client      = Client(api_key, api_secret)
 # [3] Low                       HA_High   = maximum of High, HA_Open, HA_Close
 # [4] Close                     HA_Low    = minimum of Low, HA_Open, HA_Close
 
-previous_klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, "2 hour ago UTC")
-previous.append(int(previous_klines[0][0])) # Timestamp
-previous.append(round(((float(previous_klines[0][1]) + float(previous_klines[0][4])) / 2), 2)) # Open
-previous.append(round(((float(previous_klines[0][1]) + float(previous_klines[0][2]) + float(previous_klines[0][3]) + float(previous_klines[0][4])) / 4), 2)) # Close
-previous.append(max(float(previous_klines[0][2]), previous[1], previous[2])) # High
-previous.append(min(float(previous_klines[0][3]), previous[1], previous[2])) # Low
-
-previous_Time   = int(previous_klines[0][0])
+previous_klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, "3 hour ago UTC")
+previous_Time   = int(previous_klines[1][0])
 previous_Open   = round(((float(previous_klines[0][1]) + float(previous_klines[0][4])) / 2), 2)
-previous_Close  = round(((float(previous_klines[0][1]) + float(previous_klines[0][2]) + float(previous_klines[0][3]) + float(previous_klines[0][4])) / 4), 2)
-previous_High   = max(float(previous_klines[0][2]), previous_Open, previous_Close)
-previous_Low    = min(float(previous_klines[0][3]), previous_Open, previous_Close)
+previous_Close  = round(((float(previous_klines[1][1]) + float(previous_klines[1][2]) + float(previous_klines[1][3]) + float(previous_klines[1][4])) / 4), 2)
 
-current_klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, "1 hour ago UTC")
-current.append(int(current_klines[0][0])) # Timestamp
-current.append(round(((float(previous_klines[0][1]) + float(previous_klines[0][4])) / 2), 2)) # Open
-current.append(round(((float(current_klines[0][1]) + float(current_klines[0][2]) + float(current_klines[0][3]) + float(current_klines[0][4])) / 4), 2)) # Close
-current.append(max(float(current_klines[0][2]), current[1], current[2])) # High
-current.append(min(float(current_klines[0][3]), current[1], current[2])) # Low
-
-print(current)
+print("The Initial Time is  :   " + str(previous_Time))
+print("The Initial Open is  :   " + str(previous_Open))
+print("The Initial Close is :   " + str(previous_Close) + "\n")
 
 while True:
     current_klines  = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, "1 hour ago UTC")
     current_Time    = int(current_klines[0][0])
 
-    if (current_Time - previous_Time) == 3600000:
-        current_Open  = round((previous_Open + previous_Close) / 2, 2)
-        current_Close = round((float(current_klines[0][1]) + float(current_klines[0][2]) + float(current_klines[0][3]) + float(current_klines[0][4])) / 4, 2)
-        current_High  = max(float(current_klines[0][2]), current_Open, current_Close)
-        current_Low   = min(float(current_klines[0][3]), current_Open, current_Close)
-
-        for current in current_klines:
-            print(current)
+    if (current_Time - previous_Time) == 360000:
+        temp_klines     = current_klines
+        current_Open    = round(((previous_Open + previous_Close) / 2), 2)
+        print("[>_<] previous open     : " + str(previous_Open))
+        print("[>_<] previous close    : " + str(previous_Close))
+        print("[>_<] current_Open      : " + str(current_Open) + "\n")
+        current_Close   = round(((float(current_klines[0][1]) + float(current_klines[0][2]) + float(current_klines[0][3]) + float(current_klines[0][4])) / 4), 2)
+        current_High    = max(float(current_klines[0][2]), current_Open, current_Close)
+        current_Low     = min(float(current_klines[0][3]), current_Open, current_Close)
+        
+        print("The current_Time is  :   " + str(current_Time))
+        print("The current_Open is  :   " + str(current_Open))
+        print("The current_Close is :   " + str(current_Close))
+        print("The current_High is  :   " + str(current_High))
+        print("The current_Low is   :   " + str(current_Low) + "\n")
     
     else:
-        previous_Time = current_klines
+        previous_klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, "3 hour ago UTC")
+        previous_Time   = int(previous_klines[1][0])
+        previous_Open   = round(((float(previous_klines[0][1]) + float(previous_klines[0][4])) / 2), 2)
+        previous_Close  = round(((float(previous_klines[1][1]) + float(previous_klines[1][2]) + float(previous_klines[1][3]) + float(previous_klines[1][4])) / 4), 2)
+        print("[^_^] New Candle")
 
+    print("[+] Now Sleeping...\n")
     time.sleep(10)
--- test.py
+++ test.py
@@ -15,32 +15,18 @@ api_key     = os.environ.get('API_KEY')
 api_secret  = os.environ.get('API_SECRET')
 client      = Client(api_key, api_secret)
 
-# Historical BLVT NAV Kline/Candlestick 
-# https://binance-docs.github.io/apidocs/futures/en/#taker-buy-sell-volume
-# [0] Open Timestamp            
-# [1] Open                      HA_Open   = (previous HA_Open + previous HA_Close) / 2
-# [2] High                      HA_Close  = (Open + High + Low + Close) / 4
-# [3] Low                       HA_High   = maximum of High, HA_Open, HA_Close
-# [4] Close                     HA_Low    = minimum of Low, HA_Open, HA_Close
-
-previous_klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, "2 hour ago UTC")
-previous.append(int(previous_klines[0][0])) # Timestamp
-previous.append(round(((float(previous_klines[0][1]) + float(previous_klines[0][4])) / 2), 2)) # Open
-previous.append(round(((float(previous_klines[0][1]) + float(previous_klines[0][2]) + float(previous_klines[0][3]) + float(previous_klines[0][4])) / 4), 2)) # Close
-previous.append(max(float(previous_klines[0][2]), previous[1], previous[2])) # High
-previous.append(min(float(previous_klines[0][3]), previous[1], previous[2])) # Low
-
+previous_klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, "2 minute ago UTC")
 previous_Time   = int(previous_klines[0][0])
 previous_Open   = round(((float(previous_klines[0][1]) + float(previous_klines[0][4])) / 2), 2)
 previous_Close  = round(((float(previous_klines[0][1]) + float(previous_klines[0][2]) + float(previous_klines[0][3]) + float(previous_klines[0][4])) / 4), 2)
 previous_High   = max(float(previous_klines[0][2]), previous_Open, previous_Close)
 previous_Low    = min(float(previous_klines[0][3]), previous_Open, previous_Close)
 
-current_klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, "1 hour ago UTC")
-current.append(int(current_klines[0][0])) # Timestamp
-current.append(round(((float(previous_klines[0][1]) + float(previous_klines[0][4])) / 2), 2)) # Open
-current.append(round(((float(current_klines[0][1]) + float(current_klines[0][2]) + float(current_klines[0][3]) + float(current_klines[0][4])) / 4), 2)) # Close
-current.append(max(float(current_klines[0][2]), current[1], current[2])) # High
-current.append(min(float(current_klines[0][3]), current[1], current[2])) # Low
+current_klines  = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, "1 minute ago UTC")
+current_Time    = int(current_klines[0][0])
+current_Open    = round(((previous_Open + previous_Close) / 2), 2)
+current_Close   = round(((float(current_klines[0][1]) + float(current_klines[0][2]) + float(current_klines[0][3]) + float(current_klines[0][4])) / 4), 2)
+current_High    = max(float(current_klines[0][2]), current_Open, current_Close)
+current_Low     = min(float(current_klines[0][3]), current_Open, current_Close)
 
-print(current)
+print(current_klines)
