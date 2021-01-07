f469d484fa29df35251876b0c287cbc319b0ddca
louiszhenyean <louiszhenyean@gmail.com>
2021-01-08 00:46:08 +0800

    test

--- api_keys.py
+++ api_keys.py
@@ -1,10 +1,25 @@
 # Binance Futures Testnet API
 
-def get_owner(): 
-    return "API_OWNER" 
+def get_owner():
+    return "zyairelai@gmail.com"
 
 def get_key(): 
-    return "API_KEY" 
+    return "0b6bab25b86fe60215b90a805d919eb81a330652ec10624a661300449067d3e5"
 
-def get_secret(): 
-    return "API_SECRET"
+def get_secret():
+    return "aafb5b1058ebbab777746ecce25d18606f66ab1f0234fa89129bfcd31d123d85"
+
+# Binance API for zyairelai@gmail.com
+# export API_OWNER="zyairelai"
+# export API_KEY="eEvkb2fgvUYnmP0gwlkuEoC4pcPTbK3U2qRqWACY7onOaItfI3EYt9ciaf6b0CZP"
+# export API_SECRET="6c7TZ8hWXtuvzYCAbOnDSZr5O3BIupgmZO1cvq17AKgqHWu9so7gEDOID25MvAvp"
+
+# Binance API for louiszhenyean@protonmail.com
+# export API_OWNER="louiszhenyean"
+# export API_KEY="dKFRFSdA5MEteonvEW8sYwmsRycl4MmCvuunK5ROTVsZXJ2FyKZohipPHsD1w43n"
+# export API_SECRET="Zs7vopIzhlmXYEicycTDoKDsmlC2DHCdcH347MutkK8zaulGHvtJ5MU4is0fOErY"
+
+# Binance Futures Testnet API
+# export API_OWNER="zyairelai@gmail.com"
+# export API_KEY="0b6bab25b86fe60215b90a805d919eb81a330652ec10624a661300449067d3e5"
+# export API_SECRET="aafb5b1058ebbab777746ecce25d18606f66ab1f0234fa89129bfcd31d123d85"
