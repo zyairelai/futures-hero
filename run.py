d98ada859e4d298e15428574757cfc936d43ad09
louiszhenyean <louiszhenyean@gmail.com>
2021-01-14 08:42:23 +0800

    add error

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ def get_timestamp():
     return int(time.time() * 1000)  
 
 def get_current_trend():
-    klines = client.futures_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1HOUR, limit=3)
+    klines = client.futures_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_2HOUR, limit=3)
 
     first_run_Open  = round(((float(klines[0][1]) + float(klines[0][4])) / 2), 2)
     first_run_Close = round(((float(klines[0][1]) + float(klines[0][2]) + float(klines[0][3]) + float(klines[0][4])) / 4), 2)
