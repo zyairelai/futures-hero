5708a43596d8af8c8a9df874a342972503a409d0
louiszhenyean <louiszhenyean@gmail.com>
2021-01-18 23:11:31 +0800

    Add Profit Screenshots

--- check/trend.py
+++ check/trend.py
@@ -4,51 +4,51 @@ import config
 from binance.client import Client
 
 def get_current_trend(): # >>> UP_TREND // DOWN_TREND // NO_TRADE_ZONE
-    if (get_1_hour() == "DOWN_TREND") and (get_6_hour() == "DOWN_TREND"):
-        trend = "DOWN_TREND"
-        print("Current TREND    :   🩸 DOWN_TREND 🩸")
-    elif (get_1_hour() == "UP_TREND") and (get_6_hour() == "UP_TREND"):
+    main_trend      =   get_6_hour()
+    recent_minute   =   get_30_minute()
+    
+    print("6 hour direction :   " + main_trend)
+    print("Recent 30 Minute :   " + recent_minute)
+
+    if (main_trend == "UP") and (recent_minute == "UP"):
         trend = "UP_TREND"
         print("Current TREND    :   🥦 UP_TREND 🥦")
+    elif (main_trend == "DOWN") and (recent_minute == "DOWN"):
+        trend = "DOWN_TREND"
+        print("Current TREND    :   🩸 DOWN_TREND 🩸")
     else:
         trend = "NO_TRADE_ZONE"
         print("Current TREND    :   😴 NO_TRADE_ZONE 😴")
     return trend
 
-def get_1_hour(): # >>> UP_TREND // DOWN_TREND // NO_TRADE_ZONE
-    klines = client.futures_klines(symbol=config.pair, interval=Client.KLINE_INTERVAL_1HOUR, limit=3)
-
+def get_30_minute(): # >>> UP_TREND // DOWN_TREND // NO_TRADE_ZONE
+    klines = client.futures_klines(symbol=config.pair, interval=Client.KLINE_INTERVAL_30MINUTE , limit=3)
     first_run_Open  = round(((float(klines[0][1]) + float(klines[0][4])) / 2), config.round_decimal)
     first_run_Close = round(((float(klines[0][1]) + float(klines[0][2]) + float(klines[0][3]) + float(klines[0][4])) / 4), config.round_decimal)
     previous_Open   = round(((first_run_Open + first_run_Close) / 2), config.round_decimal)
     previous_Close  = round(((float(klines[1][1]) + float(klines[1][2]) + float(klines[1][3]) + float(klines[1][4])) / 4), config.round_decimal)
-
     current_Open    = round(((previous_Open + previous_Close) / 2), config.round_decimal)
     current_Close   = round(((float(klines[2][1]) + float(klines[2][2]) + float(klines[2][3]) + float(klines[2][4])) / 4), config.round_decimal)
     current_High    = max(float(klines[2][2]), current_Open, current_Close)
     current_Low     = min(float(klines[2][3]), current_Open, current_Close)
-
-    if (current_Open == current_High):  trend = "DOWN_TREND"
-    elif (current_Open == current_Low): trend = "UP_TREND"
-    else: trend = "NO_TRADE_ZONE"
+    if      (current_Open == current_Low)   :   trend = "UP"
+    elif    (current_Open == current_High)  :   trend = "DOWN"
+    else                                    :   trend = "INDECISIVE"
     return trend
 
 def get_6_hour(): # >>> UP_TREND // DOWN_TREND // NO_TRADE_ZONE
     klines = client.futures_klines(symbol=config.pair, interval=Client.KLINE_INTERVAL_6HOUR, limit=3)
-
     first_run_Open  = round(((float(klines[0][1]) + float(klines[0][4])) / 2), config.round_decimal)
     first_run_Close = round(((float(klines[0][1]) + float(klines[0][2]) + float(klines[0][3]) + float(klines[0][4])) / 4), config.round_decimal)
     previous_Open   = round(((first_run_Open + first_run_Close) / 2), config.round_decimal)
     previous_Close  = round(((float(klines[1][1]) + float(klines[1][2]) + float(klines[1][3]) + float(klines[1][4])) / 4), config.round_decimal)
-
     current_Open    = round(((previous_Open + previous_Close) / 2), config.round_decimal)
     current_Close   = round(((float(klines[2][1]) + float(klines[2][2]) + float(klines[2][3]) + float(klines[2][4])) / 4), config.round_decimal)
     current_High    = max(float(klines[2][2]), current_Open, current_Close)
     current_Low     = min(float(klines[2][3]), current_Open, current_Close)
-
-    if (current_Open == current_High):  trend = "DOWN_TREND"
-    elif (current_Open == current_Low): trend = "UP_TREND"
-    else: trend = "NO_TRADE_ZONE"
+    if      (current_Open == current_Low)   :   trend = "UP"
+    elif    (current_Open == current_High)  :   trend = "DOWN"
+    else                                    :   trend = "INDECISIVE"
     return trend
 
 # Get environment variables
--- config.py
+++ config.py
@@ -1,64 +1,29 @@
 while True:
-    print("Here are the supported Coins: ")
-    print("1. BTC")
-    print("2. ETH")
-    print("3. BNB")
-    print("4. LINK")
-    print("5. SUSHI")
+    print("Here are the supported Pairs: ")
+    print("1. BTC-USDT")
+    print("2. ETH-USDT")
 
-    input_num = input("\nChoose your Coin :   ") or '1'
+    input_num = input("\nChoose your Pair :   ") 
 
-    if input_num == '1':
+    if (input_num == '1') or (input_num == 'btc') or (input_num == 'BTC'):
         coin            = "BTC"
-        quantity        = 0.001     # Minimum 0.001
-        leverage        = 125       # 1 - 125
+        quantity        = 0.001     # 0.001 BTC == 0.30 USDT @36XXX with leverage 125x
+        leverage        = 125       # Maximum 125
         threshold       = 0.15      # Optimal 0.15
         stoplimit       = 0.15      # shall be 70-100% of threshold
-        callbackRate    = 0.3       # Still testing
         round_decimal   = 2
         break
 
-    elif input_num == '2':
+    elif (input_num == '2') or (input_num == 'eth') or (input_num == 'ETH'):
         coin            = "ETH"
-        quantity        = 0.02      # Minimum 0.05
-        leverage        = 100       # 1 - 100
+        quantity        = 0.02      # 0.02 ETH == 0.25 USDT @12XX with leverage 100x
+        leverage        = 100       # Maximum 100
         threshold       = 0.15
         stoplimit       = 0.15
-        callbackRate    = 0.3
         round_decimal   = 2
         break
 
-    elif input_num == '3':
-        coin            = "BNB"
-        quantity        = 0.5       # Minimum 0.5
-        leverage        = 75        # 1 - 75
-        threshold       = 0.15
-        stoplimit       = 0.15
-        callbackRate    = 0.3
-        round_decimal   = 4
-        break
-
-    elif input_num == '4':
-        coin            = "LINK"
-        quantity        = 1         # Minimum 1
-        leverage        = 75        # 1 - 75
-        threshold       = 0.15
-        stoplimit       = 0.15
-        callbackRate    = 0.3
-        round_decimal   = 4
-        break
-
-    elif input_num == '5':
-        coin            = "SUSHI"
-        quantity        = 1         # Minimum 1
-        leverage        = 50        # 1 - 50
-        threshold       = 0.15
-        stoplimit       = 0.15
-        callbackRate    = 0.3
-        round_decimal   = 4
-        break
-
-    else: print("[!] Invalid Number. Try again.\n")
+    else: print("❗Invalid Number❗Try again❗\n")
 
 pair = coin + "USDT"
 
@@ -67,6 +32,5 @@ print("Trade Quantity   :   " + str(quantity) + " " + str(coin))
 print("Leverage         :   " + str(leverage) + "x")
 print("Price Movement   :   " + str(threshold) + " %")
 print("Stop Limit       :   " + str(stoplimit) + " %")
-print("Call Back Rate   :   " + str(callbackRate) + " %")
 print("Round Decimal    :   " + str(round_decimal) + " decimal place")
 print()
