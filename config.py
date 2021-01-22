61abb5517db57044bfdaa0272a36f5c15e2685f7
louiszhenyean <louiszhenyean@gmail.com>
2021-01-22 18:54:47 +0800

    fomo doesn't work

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ while True:
     if (input_pair == '1') or (input_pair == 'BTC'):
         coin            = "BTC"
         quantity        = 0.001     # Minimum 0.001
-        leverage        = 99        # Maximum 125 // Recommended 75-100
+        leverage        = 75        # Maximum 125 // Recommended 75-99
         entry_threshold = 0.15
-        exit_threshold  = 0.1
+        exit_threshold  = 0.05
         round_decimal   = 2
         break
 
     elif (input_pair == '2') or (input_pair == 'ETH'):
         coin            = "ETH"
-        quantity        = 0.02      # Minimum 0.01
-        leverage        = 75        # Maximum 100 // Recommended 50-75
+        quantity        = 0.01      # Minimum 0.01
+        leverage        = 50        # Maximum 100 // Recommended 50-75
         entry_threshold = 0.15
-        exit_threshold  = 0.1
+        exit_threshold  = 0.05
         round_decimal   = 2
         break
 
