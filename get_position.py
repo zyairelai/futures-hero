8cd0ba5c25d75dc046ae18463537d46075744f56
louiszhenyean <louiszhenyean@gmail.com>
2021-01-23 15:06:08 +0800

    show unRealizedProfit

--- get_position.py
+++ get_position.py
@@ -6,14 +6,19 @@ from termcolor import colored
 def get_position_info(): # >>> "LONGING" // "SHORTING" // "NO_POSITION"
     title = "CURRENT POSITION :   "
     positionAmt = float(binance_futures.position_information()[0].get('positionAmt'))
+    unRealizedProfit = round(float(binance_futures.position_information()[0].get('unRealizedProfit')), config.round_decimal)
 
     if (positionAmt > 0):
         position = "LONGING"
         print(colored(title + position, "green"))
+        if unRealizedProfit > 0: print(colored("unRealizedProfit :   " + str(unRealizedProfit), "green"))
+        else: print(colored("unRealizedProfit :   " + str(unRealizedProfit), "red"))
 
     elif (positionAmt < 0):
         position = "SHORTING"
         print(colored(title + position, "red"))
+        if unRealizedProfit > 0: print(colored("unRealizedProfit :   " + str(unRealizedProfit), "green"))
+        else: print(colored("unRealizedProfit :   " + str(unRealizedProfit), "red"))
 
     else:
         position = "NO_POSITION"
