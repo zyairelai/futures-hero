dca10386262c36f47e780e59b448fe2ad05a73fe
louiszhenyean <louiszhenyean@gmail.com>
2021-01-30 15:48:58 +0800

    add options for trade action

--- run.py
+++ run.py
@@ -21,7 +21,11 @@ try:
         print(colored("CHANGED LEVERAGE :   " + binance_futures.position_information()[0].get("leverage") + "x\n", "red"))
 
     def trade_action():
-        try: standard_main_hour(6)                 # standard_main_hour(hour) // fomo_trade() // double_confirm(main, support)
+        try:
+            double_confirm(6,1)
+            #fomo_trade()
+            #standard_main_hour(6)
+
         except (BinanceAPIException,
                 ConnectionResetError,
                 socket.timeout,
