d6554fcef6c54c4c7462eaf1ab84c7631c6d8efb
Zyaire <zyairelai@gmail.com>
2021-02-06 00:45:39 +0800

    strife is playing with fire

--- trade_strife.py
+++ trade_strife.py
@@ -1,4 +1,4 @@
-stoploss  = 30  # Percentage that you are willing to lose
+stoploss  = 40  # Percentage that you are willing to lose
 
 import config
 import entry_exit
@@ -20,14 +20,18 @@ def strife_with_direction():
     emergency     = get_minute.emergency_minute()
 
     if position_info == "LONGING":
-        if binance_futures.get_open_orders() == []: binance_futures.set_stop_loss("LONG", stoploss)
+        if binance_futures.get_open_orders() == []: 
+            if direction == "UP_TREND": binance_futures.set_stop_loss("LONG", stoploss)
+            else: binance_futures.set_stop_loss("LONG", stoploss/2)
         if (get_position.get_unRealizedProfit() == "PROFIT") and entry_exit.CLOSE_LONG(five_minute, emergency):
             print("ACTION           :   💰 CLOSE_LONG 💰")
             binance_futures.close_position("LONG")
         else: print(colored("ACTION           :   HOLDING_LONG", "green"))
 
     elif position_info == "SHORTING":
-        if binance_futures.get_open_orders() == []: binance_futures.set_stop_loss("SHORT", stoploss)
+        if binance_futures.get_open_orders() == []: 
+            if direction == "DOWN_TREND": binance_futures.set_stop_loss("SHORT", stoploss)
+            else: binance_futures.set_stop_loss("SHORT", stoploss/2)
         if (get_position.get_unRealizedProfit() == "PROFIT") and entry_exit.CLOSE_SHORT(five_minute, emergency):
             print("ACTION           :   💰 CLOSE_SHORT 💰")
             binance_futures.close_position("SHORT")
