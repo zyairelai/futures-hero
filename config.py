0cc636c5f657e96ff0ae9a7527ed544d3feb6ab4
Zyaire <zyairelai@gmail.com>
2021-02-08 11:11:57 +0800

    leverage 125x hehe

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ clear_direction = True      # True to minimize lose, False to maximize profit
 # Asset Configuration
 coin            = "BTC"
 quantity        = 0.003     # Minimum 0.001
-leverage        = 50        # Maximum 125 // Recommended 75-99 // Oracle 50x
+leverage        = 125       # Maximum 125 // Recommended 75-99 // Oracle 50x
 round_decimal   = 2
 
 pair = coin + "USDT"
