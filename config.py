e2b5e74fd02de5afa5c6e551430c536e5f477956
Zyaire <zyairelai@gmail.com>
2021-02-08 11:12:45 +0800

    125x dead or alive

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ clear_direction = True      # True to minimize lose, False to maximize profit
 
 # Asset Configuration
 coin            = "BTC"
-quantity        = 0.003     # Minimum 0.001
+quantity        = 0.003     # Minimum 0.003 (Must be the multiplier of 3)
 leverage        = 125       # Maximum 125 // Recommended 75-99 // Oracle 50x
 round_decimal   = 2
 
