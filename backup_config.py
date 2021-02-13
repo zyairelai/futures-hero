0cc8385033137236dcd402c1130fa78444672567
Zyaire <zyairelai@gmail.com>
2021-02-14 00:03:35 +0800

    raise leverage

--- backup_config.py
+++ backup_config.py
@@ -19,56 +19,56 @@ while True:
     if (input_pair == '1') or (input_pair == 'BTC'):
         coin            = "BTC"
         quantity        = 0.001     # Minimum 0.001, if good trending it will raise up to 3x of this amount
-        leverage        = 25        # Maximum 125 // Recommended 25-35 // Oracle 50x
+        leverage        = 50        # Maximum 125 // Recommended 25-35 // Oracle 50x
         round_decimal   = 2         # Some crypto pairs like chainlink read up to 3 decimal place of USDT
         break
 
     elif (input_pair == '2') or (input_pair == 'ETH'):
         coin            = "ETH"
         quantity        = 0.01
-        leverage        = 20        # Maximum 100 // Recommended 20-30 // Oracle 40x
+        leverage        = 40        # Maximum 100 // Recommended 20-30 // Oracle 40x
         round_decimal   = 2
         break
 
     elif (input_pair == '3') or (input_pair == 'LTC'):
         coin            = "LTC"
-        quantity        = 0.05
-        leverage        = 15
+        quantity        = 0.1
+        leverage        = 30
         round_decimal   = 2
         break
 
     elif (input_pair == '4') or (input_pair == 'BNB'):
         coin            = "BNB"
-        quantity        = 0.05
-        leverage        = 15
+        quantity        = 0.1
+        leverage        = 30
         round_decimal   = 3
         break
 
     elif (input_pair == '5') or (input_pair == 'BCH'):
         coin            = "BCH"
         quantity        = 0.01
-        leverage        = 15
+        leverage        = 30
         round_decimal   = 2
         break
 
     elif (input_pair == '6') or (input_pair == 'EOS'):
         coin            = "EOS"
-        quantity        = 2
-        leverage        = 15
+        quantity        = 5
+        leverage        = 30
         round_decimal   = 3
         break
 
     elif (input_pair == '7') or (input_pair == 'LINK'):
         coin            = "LINK"
-        quantity        = 0.5
-        leverage        = 15
+        quantity        = 1
+        leverage        = 30
         round_decimal   = 3
         break
 
     elif (input_pair == '8') or (input_pair == 'XRP'):
         coin            = "XRP"
-        quantity        = 10
-        leverage        = 15
+        quantity        = 30
+        leverage        = 30
         round_decimal   = 4
         break
 
