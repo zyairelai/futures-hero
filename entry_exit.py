7946f48955a1caa6a74ecd3d3b4ce31c06353514
Zyaire <zyairelai@gmail.com>
2021-02-17 12:49:10 +0800

    add entry exit one min condition

--- entry_exit.py
+++ entry_exit.py
@@ -1,28 +1,29 @@
 import config
 import binance_futures
-from pencil_wick import one_minute_test
+from pencil_wick import one_minute_entry_test
+from pencil_wick import one_minute_exit_test
 from pencil_wick import five_minute_test
 from pencil_wick import one_hour_test
 from time_travel import check_previous
 
 def GO_LONG(one_minute, five_minute):
-    # if ((one_minute == "GREEN") and (one_minute_test("GREEN") == "PASS")) and ((five_minute == "GREEN") and (five_minute_test("GREEN") == "PASS")): return True # Too Slow
-    if ((one_minute == "GREEN") and (one_minute_test("GREEN") == "PASS")) and \
+    # if ((one_minute == "GREEN") and (one_minute_entry_test("GREEN") == "PASS")) and ((five_minute == "GREEN") and (five_minute_test("GREEN") == "PASS")): return True # Too Slow
+    if ((one_minute == "GREEN") and (one_minute_entry_test("GREEN") == "PASS")) and \
        (((five_minute == "GREEN") or (five_minute == "GREEN_INDECISIVE")) and (five_minute_test("GREEN") == "PASS")): return True
     else: return False
 
 def GO_SHORT(one_minute, five_minute):
-    # if ((one_minute == "RED") and (one_minute_test("RED") == "PASS")) and ((five_minute == "RED") and (five_minute_test("RED") == "PASS")): return True # Too Slow
-    if ((one_minute == "RED") and (one_minute_test("RED") == "PASS")) and \
+    # if ((one_minute == "RED") and (one_minute_entry_test("RED") == "PASS")) and ((five_minute == "RED") and (five_minute_test("RED") == "PASS")): return True # Too Slow
+    if ((one_minute == "RED") and (one_minute_entry_test("RED") == "PASS")) and \
        (((five_minute == "RED") or (five_minute == "RED_INDECISIVE")) and (five_minute_test("RED") == "PASS")): return True
     else: return False
 
 def CLOSE_LONG(exit_minute):
-    if (exit_minute == "RED") or (one_minute_test("GREEN") == "FAIL"): return True
+    if (exit_minute == "RED") or (one_minute_exit_test("GREEN") == "FAIL"): return True
     else: return False
 
 def CLOSE_SHORT(exit_minute):
-    if (exit_minute == "GREEN") or (one_minute_test("RED") == "FAIL"): return True
+    if (exit_minute == "GREEN") or (one_minute_exit_test("RED") == "FAIL"): return True
     else: return False
 
 def DIRECTION_CHANGE_EXIT_LONG(one_hour):
--- pencil_wick.py
+++ pencil_wick.py
@@ -2,7 +2,47 @@ import config
 import binance_futures
 from termcolor import colored
 
-def one_minute_test(CANDLE):
+def one_minute_entry_test(CANDLE):
+    klines = binance_futures.KLINE_INTERVAL_1MINUTE()
+
+    first_run_Open  = round(((float(klines[0][1]) + float(klines[0][4])) / 2), config.round_decimal)
+    first_run_Close = round(((float(klines[0][1]) + float(klines[0][2]) + float(klines[0][3]) + float(klines[0][4])) / 4), config.round_decimal)
+    first_Open      = round(((first_run_Open + first_run_Close) / 2), config.round_decimal)
+    first_Close     = round(((float(klines[1][1]) + float(klines[1][2]) + float(klines[1][3]) + float(klines[1][4])) / 4), config.round_decimal)
+
+    previous_Open   = round(((first_Open + first_Close) / 2), config.round_decimal)
+    previous_Close  = round(((float(klines[2][1]) + float(klines[2][2]) + float(klines[1][3]) + float(klines[2][4])) / 4), config.round_decimal)
+    previous_High   = max(float(klines[2][2]), previous_Open, previous_Close)
+    previous_Low    = min(float(klines[2][3]), previous_Open, previous_Close)
+
+    current_Open    = round(((previous_Open + previous_Close) / 2), config.round_decimal)
+    current_Close   = round(((float(klines[3][1]) + float(klines[3][2]) + float(klines[3][3]) + float(klines[3][4])) / 4), config.round_decimal)
+    current_High    = max(float(klines[3][2]), current_Open, current_Close)
+    current_Low     = min(float(klines[3][3]), current_Open, current_Close)
+
+    if CANDLE == "GREEN":
+        previous_wick = previous_High - previous_Close
+        current_wick  = current_High - current_Close
+        if previous_High > current_High:
+            if current_Close > previous_Close: result = "PASS"
+            else:
+                if (previous_wick / 3) > current_wick: result = "FAIL"
+                else: result = "PASS"
+        else: result = "PASS"
+
+    elif CANDLE == "RED":
+        previous_wick = previous_Close - previous_Low
+        current_wick  = current_Close - current_Low
+        if previous_Low < current_Low:
+            if current_Close < previous_Close: result = "PASS"
+            else:
+                if (previous_wick / 3) > current_wick: result = "FAIL"
+                else: result = "PASS"
+        else: result = "PASS"
+
+    return result
+
+def one_minute_exit_test(CANDLE):
     klines = binance_futures.KLINE_INTERVAL_1MINUTE()
 
     first_run_Open  = round(((float(klines[0][1]) + float(klines[0][4])) / 2), config.round_decimal)
