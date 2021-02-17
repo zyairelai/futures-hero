d39b157d7a69f06a5a448265ab67e4fbe2be8e2d
Zyaire <zyairelai@gmail.com>
2021-02-17 11:26:35 +0800

    direction change test

--- entry_exit.py
+++ entry_exit.py
@@ -3,15 +3,18 @@ import binance_futures
 from pencil_wick import one_minute_test
 from pencil_wick import five_minute_test
 from pencil_wick import one_hour_test
+from time_travel import check_previous
 
 def GO_LONG(one_minute, five_minute):
     # if ((one_minute == "GREEN") and (one_minute_test("GREEN") == "PASS")) and ((five_minute == "GREEN") and (five_minute_test("GREEN") == "PASS")): return True # Too Slow
-    if ((one_minute == "GREEN") and (one_minute_test("GREEN") == "PASS")) and (((five_minute == "GREEN") or (five_minute == "GREEN_INDECISIVE")) and (five_minute_test("GREEN") == "PASS")): return True
+    if ((one_minute == "GREEN") and (one_minute_test("GREEN") == "PASS")) and \
+       (((five_minute == "GREEN") or (five_minute == "GREEN_INDECISIVE")) and (five_minute_test("GREEN") == "PASS")): return True
     else: return False
 
 def GO_SHORT(one_minute, five_minute):
     # if ((one_minute == "RED") and (one_minute_test("RED") == "PASS")) and ((five_minute == "RED") and (five_minute_test("RED") == "PASS")): return True # Too Slow
-    if ((one_minute == "RED") and (one_minute_test("RED") == "PASS")) and (((five_minute == "RED") or (five_minute == "RED_INDECISIVE")) and (five_minute_test("RED") == "PASS")): return True
+    if ((one_minute == "RED") and (one_minute_test("RED") == "PASS")) and \
+       (((five_minute == "RED") or (five_minute == "RED_INDECISIVE")) and (five_minute_test("RED") == "PASS")): return True
     else: return False
 
 def CLOSE_LONG(exit_minute):
@@ -23,11 +26,11 @@ def CLOSE_SHORT(exit_minute):
     else: return False
 
 def DIRECTION_CHANGE_EXIT_LONG(one_hour):
-    if ((one_hour == "RED") and (one_hour_test("RED") == "PASS")): return True
-    # if (((one_hour == "RED") or (one_hour == "INDECISIVE")) and (one_hour_test("RED") == "PASS")): return True
+    if ((one_hour == "RED") and (one_hour_test("RED") == "PASS")) or \
+       ((one_hour == "INDECISIVE") and (check_previous("1HOUR") == "GREEN")) and (one_hour_test("RED") == "PASS"): return True
     else: return False
 
 def DIRECTION_CHANGE_EXIT_SHORT(one_hour):
-    if ((one_hour == "GREEN") and (one_hour_test("GREEN") == "PASS")): return True
-    # if (((one_hour == "GREEN") or (one_hour == "INDECISIVE")) and (one_hour_test("GREEN") == "PASS")): return True
+    if ((one_hour == "GREEN") and (one_hour_test("GREEN") == "PASS")) or \
+       ((one_hour == "INDECISIVE") and (check_previous("1HOUR") == "RED") and (one_hour_test("GREEN") == "PASS")): return True
     else: return False
