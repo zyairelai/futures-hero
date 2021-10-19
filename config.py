5c7cd0374d764e69d3ee5ae71a257ff67109bab5
Jair Junior <jairpjunior@gmail.com>
2021-10-19 23:11:31 +0100

    config coins

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-live_trade = False
+live_trade = True
 
-coin     = ["BTC"]
-quantity = [0.001]
+coin     = ["DOT", "KSM", "ATOM", "HNT"]
+quantity = [3.7, 0.4, 4.3, 7]
+
 
 # profit_margin * leverage = Actual Profit Percentage.
 profit_margin = 0.4
@@ -14,7 +15,7 @@ for i in range(len(coin)):
     pair.append(coin[i] + "USDT")
     if   coin[i] == "BTC": leverage.append(50)
     elif coin[i] == "ETH": leverage.append(40)
-    else: leverage.append(30)
+    else: leverage.append(25)
 
     print("Pair Name        :   " + pair[i])
     print("Trade Quantity   :   " + str(quantity[i]) + " " + coin[i])
