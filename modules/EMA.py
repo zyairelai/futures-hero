a189df531cf621bbc81ec49c4894b012ad066573
zyairelai <zyairelai@gmail.com>
2021-12-13 10:35:12 +0800

    synchronize module

--- modules/EMA.py
+++ modules/EMA.py
@@ -1,11 +1,4 @@
-EMA_threshold = 50
-
-def apply_EMA(dataset):
-    dataset['50EMA'] = dataset['close'].ewm(span=EMA_threshold).mean()
-    dataset['trend'] = dataset.apply(identify_trend, axis=1)
-    clean = dataset[["timestamp", "50EMA", "trend"]].copy()
+def apply_EMA(dataset, EMA_threshold):
+    dataset[str(EMA_threshold) + 'EMA'] = dataset['close'].ewm(span=EMA_threshold).mean()
+    clean = dataset[["timestamp", str(EMA_threshold) + "EMA"]].copy()
     return clean
-
-def identify_trend(dataset):
-    if dataset['50EMA'] < EMA_threshold : return "UP"
-    else: return "DOWN"
