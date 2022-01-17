9f22c2f9b2ed592c3b5612e483d77fd18d30b610
Max Dobladez <elmaxi@gmail.com>
2022-01-17 12:19:44 -0300

    Add wehbook notifications

--- run.py
+++ run.py
@@ -1,18 +1,24 @@
 import api_binance
+import time
 import config
+import random
 import strategies.combined
 import strategies.ichimoku
 import strategies.volume
 import strategies.william_fractal
 import os, requests, socket, urllib3
+from webhook_launcher import telegram_bot_sendtext
 from datetime import datetime
 from termcolor import colored
 from binance.exceptions import BinanceAPIException
-print(colored("LIVE TRADE IS ENABLED\n", "green")) if config.live_trade else print(colored("THIS IS BACKTESTING\n", "red")) 
+print(colored("------ LIVE TRADE IS ENABLED ------\n", "green")) if config.live_trade else print(colored("THIS IS BACKTESTING\n", "red")) 
 
 choose_your_fighter = strategies.combined
+#fees 
+taker_fees    = 0.2
 
 def lets_make_some_money(pair, leverage, quantity): 
+    print("--------------")
     print(pair)
 
     # Retrieve Infomation for Initial Trade Setup
@@ -31,7 +37,10 @@ def lets_make_some_money(pair, leverage, quantity):
     if api_binance.LONG_SIDE(response) == "LONGING":
         if hero["EXIT_LONG"].iloc[-1] and in_Profit(response[1]):
             api_binance.market_close_long(pair, response)
-        else: print(colored("_LONG_SIDE : HOLDING_LONG", "green"))
+            print("EXIT "+str(float(response[1].get('entryPrice')))+" | Actual: "+str(float(response[1].get('markPrice')))+" | positionAmt: "+str(float(response[1].get('positionAmt')))+" | unRealizedProfit "+str(float(response[1].get('unRealizedProfit')))+"| InProfit "+str(in_Profit_show(response[1])))
+        else: 
+            print(colored("_LONG_SIDE : HOLDING_LONG", "green"))
+            print("Entry Price "+str(float(response[1].get('entryPrice')))+" | Actual: "+str(float(response[1].get('markPrice')))+" | positionAmt: "+str(float(response[1].get('positionAmt')))+" | unRealizedProfit "+str(float(response[1].get('unRealizedProfit')))+"| InProfit "+str(in_Profit_show(response[1])))
 
     if api_binance.SHORT_SIDE(response) == "NO_POSITION":
         if hero["GO_SHORT"].iloc[-1]:
@@ -41,17 +50,31 @@ def lets_make_some_money(pair, leverage, quantity):
     if api_binance.SHORT_SIDE(response) == "SHORTING":
         if hero["EXIT_SHORT"].iloc[-1] and in_Profit(response[2]):
             api_binance.market_close_short(pair, response)
-        else: print(colored("SHORT_SIDE : HOLDING_SHORT", "red"))
+            print("EXIT "+str(float(response[2].get('entryPrice')))+" | Actual: "+str(float(response[2].get('markPrice')))+" | positionAmt: "+str(float(response[2].get('positionAmt')))+" | unRealizedProfit "+str(float(response[2].get('unRealizedProfit')))+"| InProfit "+str(in_Profit_show(response[2])))
+        else: 
+            print(colored("SHORT_SIDE : HOLDING_SHORT", "red"))
+            print("Entry Price "+str(float(response[2].get('entryPrice')))+" | Actual: "+str(float(response[2].get('markPrice')))+" | positionAmt: "+str(float(response[2].get('positionAmt')))+" | unRealizedProfit "+str(float(response[2].get('unRealizedProfit')))+"| InProfit "+str(in_Profit_show(response[2])))
 
     print("Last action executed @ " + datetime.now().strftime("%H:%M:%S") + "\n")
 
 def in_Profit(response):
-    taker_fees    = 0.2
+    # taker_fees    = 0.2 changed to 0.3 to tried
+    global taker_fees
     markPrice     = float(response.get('markPrice'))
     positionAmt   = abs(float(response.get('positionAmt')))
     unRealizedPNL = round(float(response.get('unRealizedProfit')), 2)
     breakeven_PNL = (markPrice * positionAmt * taker_fees) / 100
     return True if unRealizedPNL > breakeven_PNL else False
+
+def in_Profit_show(response):
+    # taker_fees    = 0.2 changed to 0.3 to tried
+    #taker_fees    = 0.2
+    global taker_fees
+    markPrice     = float(response.get('markPrice'))
+    positionAmt   = abs(float(response.get('positionAmt')))
+    unRealizedPNL = round(float(response.get('unRealizedProfit')), 2)
+    breakeven_PNL = (markPrice * positionAmt * taker_fees) / 100
+    return round(breakeven_PNL,4)
 
 try:
     while True:
@@ -61,6 +84,7 @@ try:
                 leverage = config.leverage[i]
                 quantity = config.quantity[i]
                 lets_make_some_money(pair, leverage, quantity)
+                time.sleep(random.randint(1, 3)) #sleep to avoid penality
 
         except (socket.timeout,
                 BinanceAPIException,
@@ -75,5 +99,6 @@ try:
             with open((os.path.join("ERROR", config.pair[i] + ".txt")), "a", encoding="utf-8") as error_message:
                 error_message.write("[!] " + config.pair[i] + " - " + "Created at : " + datetime.today().strftime("%d-%m-%Y @ %H:%M:%S") + "\n" + str(e) + "\n\n")
                 print(e)
+                telegram_bot_sendtext(" ERROR RESPONSE API BINANCE: "+ str(e)+"")
 
 except KeyboardInterrupt: print("\n\nAborted.\n")
